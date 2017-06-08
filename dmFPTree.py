6e6c82a85fbb6dd6685014429d926eb71be1007d
katrinmiao <327548244@qq.com>
2017-06-08 20:57:21 +0800

    改动小点

--- dmFPTree.py
+++ dmFPTree.py
@@ -19,91 +19,91 @@ class MyEncoder(JSONEncoder): # 将数据转为json格式传到js中进行处理
     return json.JSONEncoder.default(self, obj)
 
 class treeNode:
-  def __init__(self, nameValue, numOccur, parentNode):
-    self.name = nameValue
-    self.count = numOccur
-    self.nodeLink = None
-    self.parent = parentNode    
+  def __init__(self, name_value, count_occur, parent_node):
+    self.name = name_value
+    self.count = count_occur
+    self.node_link = None
+    self.parent = parent_node    
     self.children = {} 
-  def inc(self, numOccur):
-    self.count += numOccur
+  def inc(self, count_occur):
+    self.count += count_occur
 
-def createTree(dataSet, minSup=1): # 根据数据集创建FPTree，未进行挖掘
-  headerTable = {} # 频繁项表头项目
+def createTree(data_set, min_support=1): # 根据数据集创建FPTree，未进行挖掘
+  header_table = {} # 频繁项表头项目
   # 遍历两次数据集
-  for trans in dataSet: # 第一遍遍历计算频数：统计所有项的支持度并记入频繁项表头项目表
+  for trans in data_set: # 第一遍遍历计算频数：统计所有项的支持度并记入频繁项表头项目表
     for item in trans:
-      headerTable[item] = headerTable.get(item, 0) + dataSet[trans]
+      header_table[item] = header_table.get(item, 0) + data_set[trans]
 
-  for k in list(headerTable):   # 将低于阈值的项删除
-    if headerTable[k] < minSup: 
-      del(headerTable[k])
-  freqItemSet = set(headerTable.keys()) # 频繁项集
-  if len(freqItemSet) == 0: return None, None   # 如果频繁项集为空，返回
-  for k in headerTable:
-    headerTable[k] = [headerTable[k], None] # 格式化表头
-  retTree = treeNode('Root', 1, None) # 创建树
+  for k in list(header_table):   # 将低于阈值的项删除
+    if header_table[k] < min_support: 
+      del(header_table[k])
+  freq_item_set = set(header_table.keys()) # 频繁项集
+  if len(freq_item_set) == 0: return None, None   # 如果频繁项集为空，返回
+  for k in header_table:
+    header_table[k] = [header_table[k], None] # 格式化表头
+  ret_tree = treeNode('Root', 1, None) # 创建树
 
-  for tranSet, count in dataSet.items(): # 第二遍遍历计算频数：对各个交易按照支持度排序并构建FP树
+  for tran_set, count in data_set.items(): # 第二遍遍历计算频数：对各个交易按照支持度排序并构建FP树
     localD = {}
-    for item in tranSet: # 交易项有序化
-      if item in freqItemSet:
-        localD[item] = headerTable[item][0]
+    for item in tran_set: # 交易项有序化
+      if item in freq_item_set:
+        localD[item] = header_table[item][0]
     if len(localD) > 0:
-      orderedItems = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
-      updateTree(orderedItems, retTree, headerTable, count) # 用有序的频度项集填充树
-  return retTree, headerTable  # 返回树与表头
+      ordered_items = [v[0] for v in sorted(localD.items(), key=lambda p: p[1], reverse=True)]
+      updateTree(ordered_items, ret_tree, header_table, count) # 用有序的频度项集填充树
+  return ret_tree, header_table  # 返回树与表头
 
-def updateTree(items, inTree, headerTable, count):
+def updateTree(items, inTree, header_table, count):
   if items[0] in inTree.children: # 检查有序频度项集的第一项是否存在在inTree.children
     inTree.children[items[0]].inc(count) 
   else:   # 将有序频度项集的第一项加入到inTree.children
     inTree.children[items[0]] = treeNode(items[0], count, inTree)
-    if headerTable[items[0]][1] == None: # 更新头表 
-      headerTable[items[0]][1] = inTree.children[items[0]]
+    if header_table[items[0]][1] == None: # 更新头表 
+      header_table[items[0]][1] = inTree.children[items[0]]
     else:
-      updateHeader(headerTable[items[0]][1], inTree.children[items[0]])
+      updateHeader(header_table[items[0]][1], inTree.children[items[0]])
   if len(items) > 1:  # 用剩下的有序项集更新树
-    updateTree(items[1::], inTree.children[items[0]], headerTable, count)
+    updateTree(items[1::], inTree.children[items[0]], header_table, count)
 
-def updateHeader(nodeToTest, targetNode): 
-  while (nodeToTest.nodeLink != None):  
-    nodeToTest = nodeToTest.nodeLink
-  nodeToTest.nodeLink = targetNode
+def updateHeader(node_to_test, target_node): 
+  while (node_to_test.node_link != None):  
+    node_to_test = node_to_test.node_link
+  node_to_test.node_link = target_node
 
-def ascendTree(leafNode, prefixPath):  # 从该结点向上遍历路径，构建带有最小支持度的条件模式基
-  if leafNode.parent != None:
-    prefixPath.append(leafNode.name)
-    ascendTree(leafNode.parent, prefixPath)
+def ascendTree(leaf_node, pre_path):  # 从该结点向上遍历路径，构建带有最小支持度的条件模式基
+  if leaf_node.parent != None:
+    pre_path.append(leaf_node.name)
+    ascendTree(leaf_node.parent, pre_path)
     
-def findPrefixPath(basePat, treeNode):  # 找到前缀路径 - 频繁项集的前缀也是频繁项集
-  condPats = {}
-  while treeNode != None:
-    prefixPath = []
-    ascendTree(treeNode, prefixPath)
-    if len(prefixPath) > 1: 
-      condPats[frozenset(prefixPath[1:])] = treeNode.count # frozenset：是冻结的集合，它是不可变的，一旦创建便不能更改，没有add，remove方法；存在哈希值，可以作为字典的key，也可以作为其它集合的元素。
-    treeNode = treeNode.nodeLink
-  return condPats
+def findPrefixPath(base_pattern, tree_node):  # 找到前缀路径 - 频繁项集的前缀也是频繁项集
+  condition_patterns = {}
+  while tree_node != None:
+    pre_path = []
+    ascendTree(tree_node, pre_path)
+    if len(pre_path) > 1: 
+      condition_patterns[frozenset(pre_path[1:])] = tree_node.count # frozenset：是冻结的集合，它是不可变的，一旦创建便不能更改，没有add，remove方法；存在哈希值，可以作为字典的key，也可以作为其它集合的元素。
+    tree_node = tree_node.node_link
+  return condition_patterns
 
-def mineTree(inTree, headerTable, minSup, preFix, freqItemList):
-  bigL = [v[0] for v in sorted(headerTable.items(), key=lambda p: p[1][0])] # 排序
-  for basePat in bigL:   # 从表头的底部开始
-    newFreqSet = preFix.copy()
-    newFreqSet.add(basePat)
-    print('finalFrequent Item: ',newFreqSet)    # 频繁项集
-    freqItemList.append(newFreqSet)
-    condPattBases = findPrefixPath(basePat, headerTable[basePat][1])
-    print('condPattBases : ',basePat, condPattBases)  # 条件模式基
+def mineTree(inTree, header_table, min_support, pre, freq_item_list):
+  bigL = [v[0] for v in sorted(header_table.items(), key=lambda p: p[1][0])] # 排序
+  for base_pattern in bigL:   # 有序项中的基模式
+    new_freq_set = pre.copy()
+    new_freq_set.add(base_pattern)
+    print('final_frequent Item: ',new_freq_set)    # 频繁项集
+    freq_item_list.append(new_freq_set)
+    condition_pattern_bases = findPrefixPath(base_pattern, header_table[base_pattern][1])
+    print('condition_pattern_bases : ',base_pattern, condition_pattern_bases)  # 条件模式基
     # 从条件模式基中构建条件FPTree
-    myCondTree, myHead = createTree(condPattBases, minSup)
-    str_json = json.dumps(myHead,cls=MyEncoder,indent=2)
-    if(myHead != None):
+    my_condition_tree, my_head = createTree(condition_pattern_bases, min_support)
+    str_json = json.dumps(my_head,cls=MyEncoder,indent=2)
+    if(my_head != None):
       print('head from conditional tree: ',str_json)
 
-    if myHead != None:  # 挖掘条件FPTree
-      str_json = json.dumps(myCondTree,cls=MyEncoder,indent=2)
-      mineTree(myCondTree, myHead, minSup, newFreqSet, freqItemList)
+    if my_head != None:  # 挖掘条件FPTree
+      str_json = json.dumps(my_condition_tree,cls=MyEncoder,indent=2)
+      mineTree(my_condition_tree, my_head, min_support, new_freq_set, freq_item_list)
 
 
 def loadSimpDat():
@@ -117,27 +117,27 @@ def loadSimpDat():
   '''
   data = pd.read_csv(fileName, dtype=str)
   attr = data.columns # 属性
-  attrCount = len(attr) # 属性个数
-  attrType = data.dtypes # 记录属性的类别
-  attrCov = data.cov() # 计算属性之间的协方差矩阵
-  attrCorr = data.corr() # 计算属性之间的协方差系数矩阵
-  attrBlock = data.as_blocks() # 将数据根据type分出来
-  attrMatrix = data.as_matrix()
+  # attrCount = len(attr) # 属性个数
+  # attrType = data.dtypes # 记录属性的类别
+  # attrCov = data.cov() # 计算属性之间的协方差矩阵
+  # attrCorr = data.corr() # 计算属性之间的协方差系数矩阵
+  # attrBlock = data.as_blocks() # 将数据根据type分出来
+  attr_matrix = data.as_matrix()
 
-  return attrMatrix
+  return attr_matrix
 
-def createInitSet(dataSet):
-  retDict = {}
-  for trans in dataSet:
-    retDict[frozenset(trans)] = 1
-  return retDict
+def createInitSet(data_set):
+  ret_dict = {}
+  for trans in data_set:
+    ret_dict[frozenset(trans)] = 1
+  return ret_dict
 
 
 def main():
-    simpDat = loadSimpDat()
-    minSup = int(float(sys.argv[2])/100*len(simpDat))
-    initSet = createInitSet(simpDat)
-    myFP, myHeadTable = createTree(initSet,minSup)
+    simp_data = loadSimpDat()
+    min_support = int(float(sys.argv[2])/100*len(simp_data))
+    init_set = createInitSet(simp_data)
+    myFP, my_head_table = createTree(init_set,min_support)
     str_json = json.dumps(myFP,cls=MyEncoder) # ,indent=2
     print(str_json)
 if __name__ == '__main__':
